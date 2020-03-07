db9e5d94bc056fcccb24a72d178f02456423f31c
augustczar <augustczarramos@gmail.com>
2020-03-07 16:28:19 -0300

    Criando operacao de instanciacao e calsse produto

--- src/main/java/com/augustczar/cursomc/domain/Produto.java
+++ src/main/java/com/augustczar/cursomc/domain/Produto.java
@@ -8,7 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Categoria implements Serializable {
+public class Produto implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
@@ -16,17 +16,24 @@ public class Categoria implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	private Double preco;
 	/*
-	@ManyToMany(mappedBy = "categorias")
-	private List<Produto> produtos = new ArrayList<Produto>();
+	@ManyToMany
+	@JoinTable( name = "PRODUTO_CATEGORIA",
+		joinColumns = @JoinColumn(name = "categoria_id"),
+		inverseJoinColumns = @JoinColumn(name = "categoria_id")
+	)
+	private List<Categoria> categorias = new ArrayList<Categoria>();
 	*/
-	public Categoria() {
+	public Produto() {
+		
 	}
 
-	public Categoria(Integer id, String nome) {
+	public Produto(Integer id, String nome, Double preco) {
 		super();
 		this.id = id;
 		this.nome = nome;
+		this.preco = preco;
 	}
 
 	public Integer getId() {
@@ -45,6 +52,22 @@ public class Categoria implements Serializable {
 		this.nome = nome;
 	}
 
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+/*
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
+	}
+	*/
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -61,7 +84,7 @@ public class Categoria implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Produto other = (Produto) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -69,6 +92,5 @@ public class Categoria implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
+
 }
