9e0b80ae025fe53692092a45842744c5ac97b113
augustczar <augustczarramos@gmail.com>
2020-03-14 15:44:28 -0300

    Criacao ItemPedido e ItemPedidoPK

--- src/main/java/com/augustczar/cursomc/domain/Pedido.java
+++ src/main/java/com/augustczar/cursomc/domain/Pedido.java
@@ -2,6 +2,8 @@ package com.augustczar.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -10,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -33,6 +36,10 @@ public class Pedido implements Serializable{
 	@ManyToOne
 	@JoinColumn(name = "endereco_de_entrega_id")
 	private Endereco enderecoDeEntega;
+	
+	//Set garante que eu tenha uma colecao de itens sem repeticao
+	@OneToMany(mappedBy = "id.pedido")
+	private Set<ItemPedido> itens = new HashSet<ItemPedido>();
 	
 	public Pedido() {
 	}
@@ -85,6 +92,14 @@ public class Pedido implements Serializable{
 		this.enderecoDeEntega = enderecoDeEntega;
 	}
 
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
--- src/main/java/com/augustczar/cursomc/domain/Produto.java
+++ src/main/java/com/augustczar/cursomc/domain/Produto.java
@@ -2,7 +2,10 @@ package com.augustczar.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,6 +14,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
@@ -33,6 +37,10 @@ public class Produto implements Serializable{
 	)
 	private List<Categoria> categorias = new ArrayList<Categoria>();
 	
+	//Set garante que eu tenha uma colecao de itens sem repeticao
+	@OneToMany(mappedBy = "id.produto")
+	private Set<ItemPedido> itens = new HashSet<ItemPedido>();
+	
 	public Produto() {
 		
 	}
@@ -42,6 +50,14 @@ public class Produto implements Serializable{
 		this.id = id;
 		this.nome = nome;
 		this.preco = preco;
+	}
+	
+	public List<Pedido> getPedidos(){
+		List<Pedido> lista = new ArrayList<Pedido>();
+		for (ItemPedido itemPedido : itens) {
+			lista.add(itemPedido.getPedido());
+		}
+		return lista;
 	}
 
 	public Integer getId() {
@@ -76,6 +92,14 @@ public class Produto implements Serializable{
 		this.categorias = categorias;
 	}
 	
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -100,5 +124,4 @@ public class Produto implements Serializable{
 			return false;
 		return true;
 	}
-
 }
