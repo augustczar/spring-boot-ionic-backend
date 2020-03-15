88c7454e00477f1a8cd605898aaf71c9e5bc6031
augustczar <augustczarramos@gmail.com>
2020-03-14 21:18:20 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/augustczar/cursomc/services/CategoriaService.java
+++ src/main/java/com/augustczar/cursomc/services/CategoriaService.java
@@ -23,4 +23,9 @@ public class CategoriaService {
 				"Objeto nao encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 		
 	}
+
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repository.save(obj);
+	}
 }
