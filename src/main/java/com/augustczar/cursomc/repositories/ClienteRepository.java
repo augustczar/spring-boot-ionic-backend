8cd30ad8084abda750a7101d9ffa266bc670a8ee
augustczar <augustczarramos@gmail.com>
2020-04-19 19:55:25 -0300

    Validacao customizada: Email nao repetido na insercao de Cliente

--- src/main/java/com/augustczar/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/augustczar/cursomc/repositories/ClienteRepository.java
@@ -2,10 +2,14 @@ package com.augustczar.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.augustczar.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+	
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
 
 }
--- src/main/java/com/augustczar/cursomc/services/validation/ClienteInsertAnotationValidator.java
+++ src/main/java/com/augustczar/cursomc/services/validation/ClienteInsertAnotationValidator.java
@@ -6,21 +6,29 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.augustczar.cursomc.domain.Cliente;
 import com.augustczar.cursomc.domain.enums.TipoCliente;
 import com.augustczar.cursomc.dto.ClienteNewDTO;
+import com.augustczar.cursomc.repositories.ClienteRepository;
 import com.augustczar.cursomc.resources.exceptions.FieldMessage;
 import com.augustczar.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertAnotationValidator implements ConstraintValidator<ClienteInsertAnotation, ClienteNewDTO> {
 
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsertAnotation ann) {
 	}
 
 	@Override
 	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
-
+		
 		List<FieldMessage> list = new ArrayList<>();
+		
 		// Inserindo erros customizados na lista
 		if (objDto.getTipo().equals(TipoCliente.PESSOA_FISICA.getCodigo()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
@@ -29,12 +37,19 @@ public class ClienteInsertAnotationValidator implements ConstraintValidator<Clie
 		if (objDto.getTipo().equals(TipoCliente.PESSOA_JURIDICA.getCodigo()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
+		
+		Cliente auxCliente = clienteRepository.findByEmail(objDto.getEmail());
+		
+		if (auxCliente != null) {
+			list.add(new FieldMessage("email", "Email ja existente"));
+		}
 
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
 					.addConstraintViolation();
 		}
+		
 		return list.isEmpty();
 	}
 }
