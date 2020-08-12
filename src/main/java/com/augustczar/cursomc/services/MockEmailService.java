4217252e07626975e46fc02e4d84675e4728674b
Augusto Cesar <augustczarramos@gmail.com>
2020-08-11 22:33:36 -0300

    Email html

--- src/main/java/com/augustczar/cursomc/services/MockEmailService.java
+++ src/main/java/com/augustczar/cursomc/services/MockEmailService.java
@@ -1,7 +1,10 @@
 package com.augustczar.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 
 import com.augustczar.cursomc.resources.AbstractEmailService;
@@ -16,6 +19,13 @@ public class MockEmailService  extends AbstractEmailService{
 		LOG.info(msg.toString());
 		LOG.info("Email enviado.");
 		
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Simulando envio de email HTML...");
+		LOG.info(msg.toString());
+		LOG.info("Email enviado.");
 	}
 
 }
--- src/main/java/com/augustczar/cursomc/services/SmtpEmailService.java
+++ src/main/java/com/augustczar/cursomc/services/SmtpEmailService.java
@@ -1,10 +1,13 @@
 package com.augustczar.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 import com.augustczar.cursomc.resources.AbstractEmailService;
 
@@ -12,6 +15,9 @@ public class SmtpEmailService  extends AbstractEmailService{
 	
 	@Autowired
 	private MailSender mailsender;
+	
+	@Autowired
+	private JavaMailSender javaMailSender;
 
 	private static final Logger LOG = LoggerFactory.getLogger(SmtpEmailService.class);
 
@@ -22,6 +28,14 @@ public class SmtpEmailService  extends AbstractEmailService{
 		mailsender.send(msg);
 		LOG.info("Email enviado.");
 		
+	}
+
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Enviando email...");
+		javaMailSender.send(msg);
+		LOG.info("Email enviado.");	
 	}
 
 }
