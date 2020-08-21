1e3f76fc6d3ac009e3e6207f9d94bbda7769331c
Augusto Cesar <augustczarramos@gmail.com>
2020-08-20 21:49:28 -0300

    Implementando autenticacao e geracao do token JWT

---
+++ src/main/java/com/augustczar/cursomc/security/JWTAuthenticationFilter.java
@@ -0,0 +1,84 @@
+package com.augustczar.cursomc.security;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.augustczar.cursomc.dto.CredenciaisDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
+	
+	private AuthenticationManager authenticationManager;
+	
+	private JWTUtil jwtUtil;
+	
+	//classe criada para utilizacao no spring2
+    public JWTAuthenticationFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil) {
+    	
+    	setAuthenticationFailureHandler(new JWTAuthenticationFailureHandler());
+        
+    	this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+    }
+	
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException{
+		
+		CredenciaisDTO credenciaisDTO;
+		try {
+			credenciaisDTO = new ObjectMapper()
+					.readValue(request.getInputStream(), CredenciaisDTO.class);
+			UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+					credenciaisDTO.getEmail(), credenciaisDTO.getSenha(), new ArrayList<>());
+
+			Authentication authentication = authenticationManager.authenticate(authenticationToken);
+			return authentication;
+		
+		} catch (IOException e) {
+			throw new RuntimeException();
+		}
+	}
+	
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+			Authentication auth) throws IOException, ServletException {
+		
+		//getPrincipal pega o email que eh  usuario
+		String username = ((UserSpringSecurity) auth.getPrincipal()).getUsername();
+		String token = jwtUtil.generateToken(username);
+		response.addHeader("Authorization", "Bearer " + token);
+	}
+	
+	//classe criada para utilizacao no spring2
+	private class JWTAuthenticationFailureHandler implements AuthenticationFailureHandler {
+		 
+        @Override
+        public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
+                throws IOException, ServletException {
+            response.setStatus(401);
+            response.setContentType("application/json"); 
+            response.getWriter().append(json());
+        }
+        
+        private String json() {
+            long date = new Date().getTime();
+            return "{\"timestamp\": " + date + ", "
+                + "\"status\": 401, "
+                + "\"error\": \"Não autorizado\", "
+                + "\"message\": \"Email ou senha inválidos\", "
+                + "\"path\": \"/login\"}";
+        }
+    }
+}
