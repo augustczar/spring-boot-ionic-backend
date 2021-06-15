3de20af30fb047e19e6a756a78ab69f6480e6e96
augustczar <augustczarramos@gmail.com>
2021-06-14 23:07:02 -0300

    Salvando primeiro arquivo no s3

--- src/main/java/com/augustczar/cursomc/CursomcApplication.java
+++ src/main/java/com/augustczar/cursomc/CursomcApplication.java
@@ -1,11 +1,17 @@
 package com.augustczar.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.augustczar.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication  implements CommandLineRunner{
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args){
 		SpringApplication.run(CursomcApplication.class, args);
@@ -13,7 +19,7 @@ public class CursomcApplication  implements CommandLineRunner{
 	//methodo nao utilizado no momento, deixado apena com possibilidade de ser utilizado no futuro
 	@Override
 	public void run(String... args) throws Exception {
-		
+		s3Service.uploadFile("/home/augustczar/Pictures/imgloiras/loira.png");
 	}
 
 }
